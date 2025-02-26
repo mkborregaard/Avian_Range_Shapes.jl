e3ee3552c231c64b055b1712c643967336528bab
Michael Krabbe Borregaard <mkborregaard@sund.ku.dk>
2025-02-26 10:25:33 +0100

    remove unneeded deps

--- src/AvianRangeShapes.jl
+++ src/AvianRangeShapes.jl
@@ -1,8 +1,5 @@
 module AvianRangeShapes
 
-using ArchGDAL,Rasters, DataFrames,Plots, SpreadingDye, NearestNeighbors, Images, JLD2, SkipNan
-
-
 include("functions.jl")
 
 export null_model!, prep_map, decode_range, Background, SpeciesInfo, NullModeller
